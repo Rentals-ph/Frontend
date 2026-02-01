c106c03a3344286a83e0cc3e147a75df4019f823
ianjohndal <ianjohndal5@gmail.com>
2026-02-01 16:24:50 +0800

    Enhance app build and server manifests; add agent page and layout references, update middleware configuration, and improve CSS styles for better layout and responsiveness.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2befcd12ae2231e7")
+/******/ 		__webpack_require__.h = () => ("61e836e0df4eb153")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
